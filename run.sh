fe11ac15a5996acbe30c43779e7d7812e7e464ca
Matias Cascallares <matiascas@gmail.com>
2023-12-20 16:19:23 +0100

    added run script to cover the entire demo

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Register a plain vanilla schema"
+jq -n --rawfile schema order.avsc '{schema: $schema}' | curl http://localhost:8081/subjects/orders-value/versions --header 'Content-Type: application/json' --header 'Accept: application/json' --data @-      
+
+echo "\n\nEnhancing the data contract with metadata"
+curl http://localhost:8081/subjects/orders-value/versions --header 'Content-Type: application/json' --header 'Accept: application/json' --data @order_metadata.json
+
+echo "\n\nAdding data quality rules to the data contract"
+curl http://localhost:8081/subjects/orders-value/versions --header 'Content-Type: application/json' --header 'Accept: application/json' --data @order_ruleset.json
+
+echo "\n\nDisabling a rule"
+curl http://localhost:8081/subjects/orders-value/versions --header 'Content-Type: application/json' --header 'Accept: application/json' --data @order_ruleset_disabled.json
